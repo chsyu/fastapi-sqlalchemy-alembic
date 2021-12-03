f7c7b016695d120202cf146f31eda926ba243307
chsyu <chsyu@tea.ntue.edu.tw>
2021-12-03 09:51:21 +0800

    change fastapi-sqlalchemy to sqlalchemy only

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 
 import os
+from fastapi import Depends
 from fastapi_sqlalchemy import DBSessionMiddleware
 from fastapi_sqlalchemy import db
 from models import User as ModelUser
@@ -9,27 +10,41 @@ from schema import User as SchemaUser
 from dotenv import load_dotenv
 from typing import List
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 app = FastAPI()
+db_url="postgresql+psycopg2://postgres:postgres@db:5432"
+# app.add_middleware(DBSessionMiddleware, db_url)
+engine = create_engine(db_url)
 
-app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/user", response_model=SchemaUser)
-def create_user(user: SchemaUser):
+def create_user(user: SchemaUser, db: Session = Depends(get_db)):
     db_user = ModelUser(
         first_name=user.first_name, last_name=user.last_name, age=user.age
     )
-    db.session.add(db_user)
-    db.session.commit()
+    db.add(db_user)
+    db.commit()
     return db_user
 
 
 @app.get("/users", response_model=List[SchemaUser])
-def get_all_users():
-    users = db.session.query(ModelUser).all()
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(ModelUser).all()
     if not users:
         raise HTTPException(status_code=404,
                             detail=f'Users not found')
