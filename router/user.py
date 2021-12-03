a5d6e3810407339047407cc4eed86dcc645723c2
chsyu <chsyu@tea.ntue.edu.tw>
2021-12-03 23:03:54 +0800

    correct all fault

--- router/user.py
+++ router/user.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depend
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from router.schemas import User as SchemaUser
 from db.database import get_db
 from db import db_user
+from typing import List
 
 
 router = APIRouter(
@@ -11,12 +12,12 @@ router = APIRouter(
 )
 
 
-@router.post("/user")
+@router.post("/user", response_model=SchemaUser)
 def create_user(request: SchemaUser, db: Session = Depends(get_db)):
     return db_user.create_user(request=request, db=db)
 
 
-@router.get("/users")
+@router.get("/users", response_model=List[SchemaUser])
 def get_all_users(db: Session = Depends(get_db)):
     return db_user.get_all_users(db=db)
 
